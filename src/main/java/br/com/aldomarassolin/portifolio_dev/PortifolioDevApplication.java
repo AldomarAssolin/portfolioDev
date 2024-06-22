56bd70e62d54b863315c99c9a41e7f5a762ecdfb
Aldomar Assolin <assolinaldomar@gmail.com>
2024-06-22 10:51:49 -0300

    Created project.

---
+++ src/main/java/br/com/aldomarassolin/portifolio_dev/PortifolioDevApplication.java
@@ -0,0 +1,13 @@
+package br.com.aldomarassolin.portifolio_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortifolioDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortifolioDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aldomarassolin/portifolio_dev/PortifolioDevApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aldomarassolin.portifolio_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortifolioDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
